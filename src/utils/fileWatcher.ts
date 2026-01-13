5ef47f852a9e542205e1ad7ad062b1154c2a53bc
munamadan <dipankharel@gmail.com>
2026-01-13 11:13:28 +0545

    Fix: Return disposable from file watcher for proper cleanup

--- src/utils/fileWatcher.ts
+++ src/utils/fileWatcher.ts
@@ -1,15 +1,16 @@
 import * as vscode from 'vscode';
 import { logger } from './logger';
+import { HTTP_FILE_PATTERN } from './constants';
 
 export class FileWatcher {
   private watchers: vscode.FileSystemWatcher[] = [];
-  private readonly filePattern = '**/*.{http,rest}';
+  private readonly filePattern = HTTP_FILE_PATTERN;
 
   startWatching(
     onCreated?: (uri: vscode.Uri) => void,
     onChanged?: (uri: vscode.Uri) => void,
     onDeleted?: (uri: vscode.Uri) => void
-  ): void {
+  ): vscode.Disposable {
     const watcher = vscode.workspace.createFileSystemWatcher(this.filePattern);
 
     if (onCreated) {
@@ -35,6 +36,15 @@ export class FileWatcher {
 
     this.watchers.push(watcher);
     logger.info('File watcher started');
+
+    // Return a composite disposable
+    return new vscode.Disposable(() => {
+      watcher.dispose();
+      const index = this.watchers.indexOf(watcher);
+      if (index !== -1) {
+        this.watchers.splice(index, 1);
+      }
+    });
   }
 
   dispose(): void {
